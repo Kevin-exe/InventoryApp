fb65d2bfa74fd3cabaa6ffc150ad6f04094b0bec
MetalMario137 <KevinMario117@gmail.com>
2017-11-30 14:09:00 -0500

    Merging Matt's program

--- app/src/main/java/edu/dtcc/inventoryapp/MainActivity.java
+++ app/src/main/java/edu/dtcc/inventoryapp/MainActivity.java
@@ -1,15 +1,20 @@
 package edu.dtcc.inventoryapp;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.title_screen);
+    protected void onCreate(Bundle savedInstanceState)
+    {
 
 
     }
+
 }
